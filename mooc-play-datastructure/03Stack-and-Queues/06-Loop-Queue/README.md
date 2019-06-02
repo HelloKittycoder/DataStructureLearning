7ccdb1d7d8d291000d85992521e9e02e32eaae9d
HelloKittycoder <shucheng2015@outlook.com>
2019-06-02 23:38:40 +0800

    3-6 循环队列

---
+++ mooc-play-datastructure/03Stack-and-Queues/06-Loop-Queue/README.md
@@ -0,0 +1,4 @@
+### 循环队列
+front == tail 队列为空  
+(tail + 1) % c == fromt 队列满  
+capacity中，浪费一个空间
---
+++ mooc-play-datastructure/03Stack-and-Queues/06-Loop-Queue/src/LoopQueue.java
@@ -0,0 +1,51 @@
+/**
+ * Created by shucheng on 2019-6-2 下午 23:29
+ * 循环队列的实现，不再复用动态数组，完全重写底层
+ */
+public class LoopQueue<E> implements Queue<E> {
+
+    private E[] data;
+    private int front, tail;
+    // 这个size也可以不写，直接通过front、tail计算出来；
+    // 这里暂且先多维护一个属性size，写起来稍微简单些
+    private int size;
+
+    public LoopQueue(int capacity) {
+        data = (E[]) new Object[capacity + 1];
+        front = 0;
+        tail = 0;
+    }
+
+    public LoopQueue() {
+        this(10);
+    }
+
+    public int getCapacity() {
+        return data.length - 1;
+    }
+
+    @Override
+    public int getSize() {
+        return size;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return front == size;
+    }
+
+    @Override
+    public void enqueue(E e) {
+
+    }
+
+    @Override
+    public E dequeue() {
+        return null;
+    }
+
+    @Override
+    public E getFront() {
+        return null;
+    }
+}
