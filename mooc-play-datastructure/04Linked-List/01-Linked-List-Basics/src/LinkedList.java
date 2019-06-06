d614535137146409fd2982500af7c8645d166e58
HelloKittycoder <shucheng2015@outlook.com>
2019-06-07 00:06:17 +0800

    4-1 什么是链表

---
+++ mooc-play-datastructure/04Linked-List/01-Linked-List-Basics/src/LinkedList.java
@@ -0,0 +1,28 @@
+/**
+ * Created by shucheng on 2019-6-7 上午 0:00
+ */
+public class LinkedList<E> {
+
+    private class Node {
+        public E e;
+        public Node next;
+
+        public Node(E e, Node next) {
+            this.e = e;
+            this.next = next;
+        }
+
+        public Node(E e) {
+            this(e, null);
+        }
+
+        public Node() {
+            this(null, null);
+        }
+
+        @Override
+        public String toString() {
+            return e.toString();
+        }
+    }
+}
