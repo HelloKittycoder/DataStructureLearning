3f270b48b87a4e71b8f766880c30b91e087a0981
HelloKittycoder <shucheng2015@outlook.com>
2019-06-07 16:35:41 +0800

    4-6 使用链表实现栈

---
+++ mooc-play-datastructure/04Linked-List/06-Implement-Stack-in-LinkedList/src/TestStacks.java
@@ -0,0 +1,41 @@
+import org.junit.Test;
+
+import java.util.Random;
+
+/**
+ * Created by shucheng on 2019-6-7 下午 16:25
+ * 比较数组栈和链表栈（两者算法时间复杂度相同，都是O(1)，时间差异不大）
+ */
+public class TestStacks {
+
+    private static double testStack(Stack<Integer> stack, int opCount) {
+
+        long startTime = System.nanoTime();
+
+        Random random = new Random();
+        for (int i = 0; i < opCount; i++)
+            stack.push(random.nextInt(Integer.MAX_VALUE));
+        for (int i = 0; i < opCount; i++)
+            stack.pop();
+
+        long endTime = System.nanoTime();
+
+        return (endTime - startTime) / 1000000000.0;
+    }
+
+    @Test
+    public void test() {
+
+        int opCount = 100000;
+
+        ArrayStack<Integer> arrayStack = new ArrayStack<>();
+        double time1 = testStack(arrayStack, opCount);
+        System.out.println("ArrayStack, time: " + time1 + " s");
+
+        LinkedListStack<Integer> linkedListStack = new LinkedListStack<>();
+        double time2 = testStack(linkedListStack, opCount);
+        System.out.println("LinkedListStack, time: " + time2 + " s");
+
+        // 其实这个时间比较很复杂，因为LinkedListStack中包含更多的new操作
+    }
+}
