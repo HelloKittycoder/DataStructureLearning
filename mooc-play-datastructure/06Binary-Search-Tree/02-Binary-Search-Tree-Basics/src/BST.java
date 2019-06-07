3a64d5e2817ead86f82b97437a3f606273c92f1e
HelloKittycoder <shucheng2015@outlook.com>
2019-06-07 23:31:20 +0800

    6-2 二分搜索树基础

---
+++ mooc-play-datastructure/06Binary-Search-Tree/02-Binary-Search-Tree-Basics/src/BST.java
@@ -0,0 +1,33 @@
+/**
+ * Created by shucheng on 2019-6-7 下午 23:24
+ * 二分搜索树
+ */
+public class BST<E extends Comparable<E>> {
+
+    private class Node {
+        public E e;
+        public Node left, right;
+
+        public Node(E e) {
+            this.e = e;
+            left = null;
+            right = null;
+        }
+    }
+
+    private Node root;
+    private int size;
+
+    public BST() {
+        root = null;
+        size = 0;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+}
