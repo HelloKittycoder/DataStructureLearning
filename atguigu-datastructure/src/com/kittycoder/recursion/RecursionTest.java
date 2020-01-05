6c98f6b9b82c3e460a04b53ea6768b487315aa1b
HelloKittycoder <shucheng2015@outlook.com>
2020-01-05 22:45:28 +0800

    递归测试

---
+++ atguigu-datastructure/src/com/kittycoder/recursion/RecursionTest.java
@@ -0,0 +1,45 @@
+package com.kittycoder.recursion;
+
+import org.junit.Test;
+
+/**
+ * Created by shucheng on 2020/1/5 21:45
+ * 递归测试
+ */
+public class RecursionTest {
+
+    @Test
+    public void test1() {
+        // test(4, 0);
+        System.out.println(factorial(3));
+    }
+
+    // 原本的test递归方法
+    // 注意加else和不加else的区别
+    public static void test(int n) {
+        if (n > 2) {
+            test(n - 1);
+        } // else {
+            System.out.println("n=" + n);
+        // }
+    }
+
+    // 测试test递归方法，加了generateDepthString
+    public static void test(int n, int depth) {
+        System.out.println(RecursionUtil.generateDepthString(depth));
+        if (n > 2) {
+            test(n - 1, depth + 1);
+        }
+        System.out.println("n=" + n);
+        System.out.println(RecursionUtil.generateDepthString(depth));
+    }
+
+    // 阶乘问题
+    public static int factorial(int n) {
+        if (n == 1) {
+            return 1;
+        } else {
+            return factorial(n - 1) * n;
+        }
+    }
+}
