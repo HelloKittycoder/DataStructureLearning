5c386d35320ee913071ca4198fcc0bc4a1dc634c
HelloKittycoder <shucheng2015@outlook.com>
2020-01-21 17:54:12 +0800

    排序-希尔排序（采用移位法）

--- atguigu-datastructure/src/com/kittycoder/sort/ShellSort.java
+++ atguigu-datastructure/src/com/kittycoder/sort/ShellSort.java
@@ -184,4 +184,23 @@ public class ShellSort {
             }
         }
     }
+
+    // 使用移位法对希尔排序进行优化（课程里的写法，这个和shellSort其实是一样的）
+    public static void shellSort3(int[] arr) {
+        // 增量gap，并逐步缩小增量
+        for (int gap = arr.length / 2; gap > 0; gap /= 2) {
+            // 从第gap个元素，逐个对其所在的组进行直接插入排序
+            for (int i = gap; i < arr.length; i++) {
+                int j = i;
+                int temp = arr[j];
+                while (j - gap >= 0 && temp < arr[j - gap]) {
+                    // 移动
+                    arr[j] = arr[j - gap];
+                    j -= gap;
+                }
+                // 当退出while后，就给temp找到插入的位置
+                arr[j] = temp;
+            }
+        }
+    }
 }
