64dca6450e33cb6f1f12b0f76108dbd15fcef025
HelloKittycoder <shucheng2015@outlook.com>
2021-06-07 23:57:36 +0800

    顺序存储二叉树中序、后序遍历

--- atguigu-datastructure/src/main/java/com/kittycoder/tree/ArrBinaryTreeDemo.java
+++ atguigu-datastructure/src/main/java/com/kittycoder/tree/ArrBinaryTreeDemo.java
@@ -10,6 +10,8 @@ public class ArrBinaryTreeDemo {
         //创建一个 ArrBinaryTree
         ArrBinaryTree binaryTree = new ArrBinaryTree(arr);
         binaryTree.preOrder(); // 1,2,4,5,3,6,7
+        binaryTree.infixOrder(); // 4,2,5,1,6,3,7
+        binaryTree.postOrder(); // 4,5,2,6,7,3,1
     }
 }
 
@@ -47,5 +49,57 @@ class ArrBinaryTree {
         if (2 * index + 2 < arr.length) {
             preOrder(2 * index + 2);
         }
+    }
+
+    public void infixOrder() {
+        infixOrder(0);
+    }
+
+    /**
+     * 编写一个方法，完成顺序存储二叉树的中序遍历
+     * @param index 数组的下标
+     */
+    public void infixOrder(int index) {
+        // 如果数组为空，或者 arr.length = 0
+        if (arr == null || arr.length == 0) {
+            System.out.println("数组为空，不能进行二叉树的中序遍历");
+            return;
+        }
+        // 向左递归遍历
+        if (2 * index + 1 < arr.length) {
+            infixOrder(2 * index + 1);
+        }
+        // 输出当前这个元素
+        System.out.println(arr[index]);
+        //向右递归遍历
+        if (2 * index + 2 < arr.length) {
+            infixOrder(2 * index + 2);
+        }
+    }
+
+    public void postOrder() {
+        postOrder(0);
+    }
+
+    /**
+     * 编写一个方法，完成顺序存储二叉树的后序遍历
+     * @param index 数组的下标
+     */
+    public void postOrder(int index) {
+        // 如果数组为空，或者 arr.length = 0
+        if (arr == null || arr.length == 0) {
+            System.out.println("数组为空，不能进行二叉树的后序遍历");
+            return;
+        }
+        // 向左递归遍历
+        if (2 * index + 1 < arr.length) {
+            postOrder(2 * index + 1);
+        }
+        //向右递归遍历
+        if (2 * index + 2 < arr.length) {
+            postOrder(2 * index + 2);
+        }
+        // 输出当前这个元素
+        System.out.println(arr[index]);
     }
 }
