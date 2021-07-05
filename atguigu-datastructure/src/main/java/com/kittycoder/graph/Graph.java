c73004276970fcd7eeadb9930f934811a2aecd05
HelloKittycoder <shucheng2015@outlook.com>
2021-07-05 23:50:31 +0800

    图的深度优先（DFS）代码实现

--- atguigu-datastructure/src/main/java/com/kittycoder/graph/Graph.java
+++ atguigu-datastructure/src/main/java/com/kittycoder/graph/Graph.java
@@ -12,6 +12,7 @@ public class Graph {
     private List<String> vertexList; // 存储顶点集合
     private int[][] edges; // 存储图对应的邻接矩阵
     private int numOfEdge; // 表示边的数目
+    private boolean[] isVisited; // 记录已访问过的节点
 
     public static void main(String[] args) {
         String[] vertexArr = {"A", "B", "C", "D", "E"};
@@ -31,6 +32,9 @@ public class Graph {
         graph.insertEdge(1, 4, 1);
 
         graph.showGraph();
+
+        System.out.println("深度优先");
+        graph.dfs();
     }
 
     public Graph(int n) {
@@ -38,6 +42,58 @@ public class Graph {
         vertexList = new ArrayList<>();
         edges = new int[n][n];
         numOfEdge = 0;
+        isVisited = new boolean[n];
+    }
+
+    /**
+     * 获取某个节点的第一个邻接节点的下标w
+     * @param index
+     * @return 如果存在就返回对应的下标，否则返回-1
+     */
+    public int getFirstNeighbor(int index) {
+        for (int j = 0; j < getNumOfVertex(); j++) {
+            if (edges[index][j] > 0) {
+                return j;
+            }
+        }
+        return -1;
+    }
+
+    // 根据前一个邻接节点的下标获取下一个邻接节点
+    public int getNextNeighbor(int v1, int v2) {
+        for (int j = v2 + 1; j < getNumOfVertex(); j++) {
+            if (edges[v1][j] > 0) {
+                return j;
+            }
+        }
+        return -1;
+    }
+
+    // 深度优先遍历算法
+    private void dfs(boolean[] isVisited, int i) {
+        System.out.print(getValueByIndex(i) + "->");
+        // 标记节点v为已访问
+        isVisited[i] = true;
+        // 查找节点v的第一个邻接节点w
+        int w = getFirstNeighbor(i);
+        while (w != -1) {
+            // 若w未被访问，对w进行深度优先遍历递归
+            if (!isVisited[w]) {
+                dfs(isVisited, w);
+            }
+            // 如果w已被访问，则查找节点v的邻接节点w的下一个邻接节点
+            w = getNextNeighbor(i, w);
+        }
+        // 如果w不存在，则从v的下一个节点继续（这个在下一次for循环进来，从dfs()方法过来的）
+    }
+
+    // 对dfs进行重载，遍历所有的节点，进行dfs操作
+    public void dfs() {
+        for (int i = 0; i < getNumOfVertex(); i++) {
+            if (!isVisited[i]) {
+                dfs(isVisited, i);
+            }
+        }
     }
 
     // 图中常用的方法
